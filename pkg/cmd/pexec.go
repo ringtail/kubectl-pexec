094c39962c1ef67881e75a7ee555ac97a447f1ad
ringtail <zhongwei.lzw@alibaba-inc.com>
2020-01-31 19:03:07 +0800

    add init branch

--- pkg/cmd/pexec.go
+++ pkg/cmd/pexec.go
@@ -7,6 +7,15 @@ import (
 	"errors"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sync"
+	"github.com/ringtail/kubectl-pexec/pkg/util"
+	"k8s.io/client-go/rest"
+	"strings"
+	"os"
+	"path"
 )
 
 const (
@@ -38,11 +47,15 @@ func NewPExecCommand(streams genericclioptions.IOStreams) *cobra.Command {
 			return nil
 		},
 	}
+
+	o.configFlags.AddFlags(cmd.Flags())
+
 	return cmd
 }
 
 type PExecOptions struct {
 	configFlags  *genericclioptions.ConfigFlags
+	restConfig   *rest.Config
 	args         []string
 	workloadType string
 	genericclioptions.IOStreams
@@ -56,11 +69,11 @@ func (peo *PExecOptions) Complete(c *cobra.Command, args []string) (err error) {
 func (peo *PExecOptions) Validate() (err error) {
 	args := peo.args
 
-	if len(args) == 0 {
+	if len(args) < 4 {
 		return errors.New("NoneValidArgs")
 	}
 
-	workloadType := args[0]
+	workloadType := args[1]
 
 	switch workloadType {
 	case "deployment", "deploy":
@@ -79,16 +92,107 @@ func (peo *PExecOptions) Validate() (err error) {
 }
 
 func (peo *PExecOptions) Run() (err error) {
-	config, err := clientcmd.BuildConfigFromFlags("", "")
+
+	kubeconf := *peo.configFlags.KubeConfig
+
+	home, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = kubernetes.NewForConfig(config)
+	udc := path.Join(home, ".kube", "config")
+
+	if _, err := os.Stat(udc); kubeconf == "" && err == nil {
+		kubeconf = udc
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconf)
 	if err != nil {
 		panic(err)
 	}
 
+	peo.restConfig = config
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	namespace := peo.configFlags.Namespace
+	if *namespace == "" {
+		*namespace = "default"
+	}
+
+	pods, err := peo.GetPods(clientSet, namespace)
+
+	if err != nil {
+		return err
+	}
+
+	err = peo.Pexec(clientSet, namespace, pods)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (peo *PExecOptions) GetPods(clientSet *kubernetes.Clientset, namespace *string) (pods []v1.Pod, err error) {
+	workloadName := peo.args[2]
+
+	matchLabels := make(map[string]string)
+	switch peo.workloadType {
+	case "Deployment":
+		deploy, err := clientSet.AppsV1().Deployments(*namespace).Get(workloadName, metav1.GetOptions{})
+		if err != nil {
+			// handle error
+			return pods, err
+		}
+		matchLabels = deploy.GetLabels()
+	case "StatefulSet":
+		statefulSet, err := clientSet.AppsV1().StatefulSets(*namespace).Get(workloadName, metav1.GetOptions{})
+		if err != nil {
+			// handle error
+			return pods, err
+		}
+		matchLabels = statefulSet.GetLabels()
+	case "DaemonSet":
+		daemonSet, err := clientSet.AppsV1().DaemonSets(*namespace).Get(workloadName, metav1.GetOptions{})
+		if err != nil {
+			// handle error
+			return pods, err
+		}
+		matchLabels = daemonSet.GetLabels()
+	default:
+		//
+		return pods, errors.New("UnknownWorkloadType")
+	}
+
+	podList, err := clientSet.CoreV1().Pods(*namespace).List(metav1.ListOptions{
+		LabelSelector: labels.SelectorFromSet(labels.Set(matchLabels)).String(),
+	})
+
+	if err != nil {
+		// handle error
+		return pods, err
+	}
+
+	return podList.Items, nil
+}
+
+func (peo *PExecOptions) Pexec(clientSet *kubernetes.Clientset, namespace *string, pods []v1.Pod) (err error) {
+
+	wg := &sync.WaitGroup{}
+	for index, _ := range pods {
+		wg.Add(1)
+		go func(pod *v1.Pod, clientSet *kubernetes.Clientset, wg *sync.WaitGroup) {
+			if err != nil {
+				panic(err)
+			}
+			util.Execute(clientSet, namespace, peo.restConfig, pod.Name, strings.Join(peo.args[3:], " "), peo.IOStreams.In, peo.IOStreams.Out, peo.IOStreams.ErrOut)
+			wg.Done()
+		}(&pods[index], clientSet, wg)
+	}
+	wg.Wait()
 	return nil
 }
 
---
+++ pkg/util/util.go
@@ -0,0 +1,55 @@
+package util
+
+import (
+	"io"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+)
+
+func Execute(client kubernetes.Interface, namespace *string, config *restclient.Config, podName string, command string, stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+	cmd := []string{
+		"sh",
+		"-c",
+		command,
+	}
+
+	req := client.CoreV1().RESTClient().Post().Resource("pods").Name(podName).
+		Namespace(*namespace).SubResource("exec")
+	option := &v1.PodExecOptions{
+		Command: cmd,
+		Stdin:   true,
+		Stdout:  true,
+		Stderr:  true,
+		TTY:     true,
+	}
+
+	if stdin == nil {
+		option.Stdin = false
+	}
+
+	req.VersionedParams(
+		option,
+		scheme.ParameterCodec,
+	)
+
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	if err != nil {
+		return err
+	}
+
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
